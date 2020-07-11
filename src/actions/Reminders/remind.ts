7094c3cad44c1f038c9ddd8afe83f460a2518f1d
manuel-di-iorio <manuel.diiorio.mail@gmail.com>
2020-07-11 13:27:25 +0200

    Reminders: am/pm question

--- src/actions/Reminders/remind.ts
+++ src/actions/Reminders/remind.ts
@@ -63,7 +63,14 @@ export default {
     }
 
     // Parse the date
-    let remindDate = new Date(moment(remindWhen, ['HH', 'HH:mm', 'HH:mm:ss', 'HH:mm:ss DD', 'HH:mm:ss DD/MM', 'HH:mm:ss DD/MM/YYYY', 'DD/MM', 'DD/MM/YYYY'], true).valueOf())
+    let remindDate = new Date(moment(remindWhen, [
+      'H', 'HH', 'H:mm', 'HH:mm', 'H:mm:ss', 'HH:mm:ss', 'DD/MM',
+      'H:mm:ss D', 'HH:mm:ss D', 'HH:mm:ss DD',
+      'H:mm:ss DD/MM', 'HH:mm:ss DD/MM', 'HH:mm:ss DD/M',
+      'H:mm:ss DD/MM/YYYY', 'H:mm:ss D/MM/YYYY', 'HH:mm:ss D/MM/YYYY', 'HH:mm:ss DD/MM/YYYY',
+      'HH:mm:ss DD/M/YYYY', 'H:mm:ss DD/M/YYYY', 'H:mm:ss D/M/YYYY', 'HH:mm:ss D/M/YYYY',
+      'D/MM/YYYY', 'D/M/YYYY', 'DD/MM/YYYY'
+    ], true).valueOf())
 
     if (!(remindDate instanceof Date) || isNaN(+remindDate)) {
       remindDate = parseNaturalDate(remindWhen)
@@ -71,11 +78,36 @@ export default {
 
     if (!remindDate) return reply('non ho capito la data. Remind annullato')
 
-    const now = Date.now()
+    // Ask AM/PM time if needed
+    const today = new Date()
+    const timeHours = remindDate.getHours()
+
+    if (timeHours < 13) {
+      // Ask the question
+      const articleHours = timeHours > 1 ? 'le' : "l'"
+      const articleMorningText = timeHours < 7 ? 'di' : 'del'
+      const morningText = timeHours < 7 ? 'notte' : 'mattino'
+      const eveningText = timeHours < 7 ? 'pomeriggio' : 'sera'
+      const answer = await askMsgReply(message, { text: `${articleHours} ${timeHours} ${articleMorningText} ${morningText} o ${eveningText} ?` })
+      if (!answer) return
+
+      // Calculate the fixed date based on the answer
+      const answerClean = answer.toLowerCase().trim()
+      if (answerClean !== 'mattino' && answerClean !== 'mattina' && answerClean !== 'pomeriggio' && answerClean !== 'sera' && answerClean !== 'notte') {
+        return reply('non ho capito la risposta. Remind annullato')
+      } else if (answerClean === 'pomeriggio' || answerClean === 'sera') {
+        remindDate.setHours(timeHours + 12)
+      } else if (remindDate.getDate() === today.getDate() && (answerClean === 'mattina' || answerClean === 'mattino' || answerClean === 'notte')) {
+        remindDate.setHours(timeHours + 24)
+      }
+    }
+
+    // Date check
+    const todayTimestamp = today.getTime()
     const remindDateTime = remindDate.getTime()
-    if (remindDateTime < now) {
+    if (remindDateTime < todayTimestamp) {
       return reply('la data del reminder deve essere nel futuro. Remind annullato')
-    } else if (remindDateTime - now > 31536e6 * 10 /* 10 years */) {
+    } else if (remindDateTime - todayTimestamp > 31536e6 * 10 /* 10 years */) {
       return reply('il massimo è 10 anni. Remind annullato')
     }
 
