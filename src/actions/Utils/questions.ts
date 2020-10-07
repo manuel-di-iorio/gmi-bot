a6cef2949fa3730a2dc35f0b27c66268dc73af83
manuel-di-iorio <manuel.diiorio.mail@gmail.com>
2020-10-07 18:57:08 +0200

    feat(!questions)

--- src/actions/Utils/questions.ts
+++ src/actions/Utils/questions.ts
@@ -1,4 +1,3 @@
-import logger from '../../lib/Logger'
 import { Task } from '../../lib/Queue'
 import { NEWLINE } from '../../lib/utils/GetNewline'
 
@@ -6,11 +5,6 @@ export default {
   cmd: 'questions',
 
   handler: async ({ message }: Task) => {
-    // Delete the author message
-    if (message.guild) {
-      message.delete().catch((err: Error) => logger.error(err))
-    }
-
     await message.channel.send(`1) Conosci Game Maker ?${NEWLINE}2) Conosci Peggle ?${NEWLINE}3) Usi Unity ?${NEWLINE}4) Giochi a League of Legends ?`)
   }
 }
