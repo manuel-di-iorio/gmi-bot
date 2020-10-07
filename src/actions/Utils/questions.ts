267a97b0c4478711633d8b7c1f6c46f8a870f46d
manuel-di-iorio <manuel.diiorio.mail@gmail.com>
2020-10-07 19:00:18 +0200

    feat(!questions)

--- src/actions/Utils/questions.ts
+++ src/actions/Utils/questions.ts
@@ -5,6 +5,6 @@ export default {
   cmd: 'questions',
 
   handler: async ({ message }: Task) => {
-    await message.channel.send(`1) Conosci Game Maker ?${NEWLINE}2) Conosci Peggle ?${NEWLINE}3) Usi Unity ?${NEWLINE}4) Giochi a League of Legends ?`)
+    await message.channel.send(`1) Conosci Game Maker?${NEWLINE}2) Conosci Peggle?${NEWLINE}3) Usi Unity?${NEWLINE}4) Giochi a League of Legends?`)
   }
 }
